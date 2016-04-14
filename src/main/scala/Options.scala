6d70b774fe11e163cd0b27c3cea69c3e78ec6ca6
Steve Nguyen <vnsgbt@me.com>
2016-04-14 10:39:59 -0500

    add Option

---
+++ src/main/scala/Options.scala
@@ -0,0 +1,78 @@
+/**
+  * Created by snguy on 4/14/2016.
+  */
+object Options extends App {
+
+  val greeting: Option[String] = Some("Hello world!")
+  val greetingNone: Option[String] = None
+
+  val user1 = userRepo.findId(1)
+
+  if (user1.isDefined) println ("\nClunky way: \n" + user1 + "\n name: " + user1.get.first)
+
+  val user2: User = userRepo.findId(2).getOrElse(User(2,"a","b",3,None))
+
+  println("\nfallback way: \n Gender: " + user2.gender.getOrElse("Not specified"))
+
+  val gender = user2.gender match {
+    case Some(gender) => "Gender: " + gender
+    case None => "Gender: not specified"
+  }
+
+  println("\nOption pattern matching since Some is also a case: \n" + gender)
+
+  userRepo.findId(2).foreach(user => println("\nOption is like collection: \n"
+    + user.gender.getOrElse("notSpecified")))
+
+  println("\nOption can be mapped: \n" +
+    userRepo.findId(1).map(_.age))
+
+  println("\nMapping an Option to Option is chained: \n + " +
+    userRepo.findId(1).map(_.gender))
+
+  println("\nUsing flatMap: \n" +
+    userRepo.findId(1).flatMap(_.gender) + "\n" +
+    userRepo.findId(2).flatMap(_.gender) + "\n" +
+    userRepo.findId(3).flatMap(_.gender) + "\n"
+  )
+  println("\nUsing filter: \n" +
+    userRepo.findId(1).filter(_.age > 30) + "\n" +
+    userRepo.findId(2).filter(_.age > 30) + "\n" +
+    userRepo.findId(3).filter(_.age > 30) + "\n"
+  )
+
+  println("\nComprehension: \n" +
+    (for {
+      user <- userRepo.findId(1)
+      gender <- user.gender
+    } yield gender))
+  println("\nComprehension flat: \n" +
+    (for {
+      user <- userRepo.findAll
+      gender <- user.gender
+    } yield gender))
+  println("\nLeft side generator: \n" +
+    (for {
+      User(_,_,_,_,Some(gender)) <- userRepo.findAll
+    } yield gender))
+
+
+  case class Resource (content: String)
+  val resourceFromConfig: Option[Resource] = None
+  val resourceFromPath: Option[Resource] = Some(Resource("I'm a resource."))
+
+  println("\nChaining Options: \n" +
+    (resourceFromConfig orElse resourceFromPath)
+  )
+}
+
+case class User (id: Int, first: String, last: String, age: Int, gender: Option[String])
+
+object userRepo {
+
+  private val users = Map(1 -> User(1, "John", "Doe", 32, Some("male")),
+                          2 -> User(2, "Johana", "Duh", 30, None))
+
+  def findId (id: Int): Option[User] = users.get(id)
+  def findAll = users.values
+}
--- src/main/scala/extractor/Anonymous.scala
+++ src/main/scala/extractor/Anonymous.scala
@@ -4,7 +4,7 @@ package extractor
   * Created by snguy on 4/13/2016.
   */
 
-object Anonymous {
+object Anonymous extends App {
 
   val wordFreqs = ("habitual", 6) :: ("and", 56) :: ("consuetudinary", 2) ::
     ("additionally", 27) :: ("homely", 5) :: ("society", 13) :: Nil
@@ -14,4 +14,25 @@ object Anonymous {
 
   val predicate: ((String, Int)) => Boolean = { case (_, f) => f > 3 && f < 25}
   val transform: ((String, Int)) => String = { case (w, _) => w}
+
+  /**
+    * Partial Function
+    * */
+
+
+  val pf = new PartialFunction[(String, Int), String] {
+    def apply (wordFreq: (String, Int)) = wordFreq match {
+      case (word, freq) if freq > 2 && freq < 25 => word
+    }
+    def isDefinedAt(x: (String, Int)): Boolean = x match {
+      case (word, freq) if freq > 3 && freq < 25 => true
+      case _ => false
+    }
+  }
+
+  println("Seq's collect: " + wordFreqs.collect(pf))
+
+  println("partialFunc: " + wordFreqs.collect {
+    case (word, freq) if freq > 3 && freq < 25 => word
+  })
 }
