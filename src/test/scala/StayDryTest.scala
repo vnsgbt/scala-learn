15cf1cc9d22080b2d3a69f748cdc77827a17b539
steve nguyen <vnsgbt@me.com>
2016-04-30 21:46:40 -0500

    add pipeline

--- src/test/scala/StayDryTest.scala
+++ src/test/scala/StayDryTest.scala
@@ -21,4 +21,17 @@ class StayDryTest extends UnitSpec {
     recipient = "me@me.me") :: Nil
 
   assert(StayDry.newMailForUser(mails, emailFilter) == Seq.empty)
+
+  val addMissingSubject = (email: Email) =>
+    if (email.subject.isEmpty) email.copy(subject = "No subject") else email
+  val checkSpelling = (email: Email) =>
+    email.copy(text = email.text.replaceAll("your","you're"))
+  val removeOffensiveLanguage = (email: Email) =>
+    email.copy(text = email.text.replaceAll("Dynamic typing","**CENSORED"))
+  val addAdToFooter = (email: Email) =>
+    email.copy(text = email.text + "\nThis email sent via Super Mail")
+
+  val pipeline = Function.chain(Seq(
+    addMissingSubject, checkSpelling, removeOffensiveLanguage, addAdToFooter
+  ))
 }
